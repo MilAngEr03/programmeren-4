07b3842b45715830a012367e3cfc04cd3c1d41dd
MilAngEr03 <milan.bollebakker@icloud.com>
2024-06-13 15:20:17 +0200

    imported database connection and templates for routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const assert = require('assert')
+const router = express.Router()
+const userController = require('../controllers/user.controller')
+const logger = require('../util/logger')
+
+// Tijdelijke functie om niet bestaande routes op te vangen
+const notFound = (req, res, next) => {
+    next({
+        status: 404,
+        message: 'Route not found',
+        data: {}
+    })
+}
+
+// Userroutes
+router.post('/api/user', validateUserCreate, userController.create)
+router.get('/api/user', userController.getAll)
+router.get('/api/user/profile', validateToken, userController.getProfile)
+router.get('/api/user/:userId', userController.getById)
+
+// Tijdelijke routes om niet bestaande routes op te vangen
+router.put('/api/user/:userId', notFound)
+router.delete('/api/user/:userId', notFound)
+
+module.exports = router
