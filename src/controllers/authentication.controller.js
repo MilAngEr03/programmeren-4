63b8c8c122117bc657b6984553c32c195e342b84
MilAngEr03 <milan.bollebakker@icloud.com>
2024-06-26 10:47:42 +0200

    api authentication fixed

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -24,6 +24,25 @@ const authController = {
                 })
             }
         })
+    },
+    info: (req, res, next) => {
+        logger.debug('info')
+        authService.login((error, success) => {
+            if (error) {
+                return next({
+                    status: error.status,
+                    message: error.message,
+                    data: {}
+                })
+            }
+            if (success) {
+                res.status(200).json({
+                    status: success.status,
+                    message: success.message,
+                    data: ("Milan Bollebakker \n2217058 \nDit is een api server voor een maaltijd planner. ")
+                })
+            }
+        })
     }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -16,10 +16,10 @@ const notFound = (req, res, next) => {
 
 // Userroutes
 router.post('/api/user', userController.create)
-router.get('/api/user', userController.getAll)
+router.get('/api/user', validateToken, userController.getAll)
 router.get('/api/user/profile', validateToken, userController.getProfile)
-router.get('/api/user/:userId', userController.getById)
-router.put('/api/user/:userId', userController.update)
+router.get('/api/user/:userId', validateToken, userController.getById)
+router.put('/api/user/:userId', validateToken, userController.update)
 
 // Tijdelijke routes om niet bestaande routes op te vangen
 router.delete('/api/user/:userId', notFound)
